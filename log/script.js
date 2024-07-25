d52e41b004f0063596a501b89e2b4565e4bfaf2f
Aymen Loudiy <loudiyamen@gmail.com>
2024-07-25 10:48:38 +0100

    test

--- log/script.js
+++ log/script.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById('loginForm');
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            window.location.href = "./Dashboard/dashboard.html";
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      window.location.href = "./Dashboard/dashboard.html";
+      console.log(event);
+    });
+  }
 });
-
-
